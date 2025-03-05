a73b0a9288ae544f76c19d0d327cccf68f7633ea
Sergey Zenichev <s_zenichev@baumanracing.ru>
2025-03-05 17:59:16 +0300

    First sucsessfull robot spawn

--- src/puma560_description/launch/publish_urdf.launch.py
+++ src/puma560_description/launch/publish_urdf.launch.py
@@ -5,19 +5,46 @@ from launch import LaunchDescription
 from launch.substitutions import Command
 from launch_ros.actions import Node
 
+# this is the function launch  system will look for
 def generate_launch_description():
 
-    robot_description_path = os.path.join(get_package_share_directory("puma560_description"),"/urdf/puma560_robot.urdf")
+    ####### DATA INPUT ##########
+    urdf_file = 'puma560_robot.urdf'
+    #xacro_file = "box_bot.xacro"
+    package_description = "puma560_description"
+
+    ####### DATA INPUT END ##########
+    print("Fetching URDF ==>")
+    robot_desc_path = os.path.join(get_package_share_directory(package_description), "urdf", urdf_file)
+
+    # Robot State Publisher
 
     robot_state_publisher_node = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        name="robot_state_publisher_node",
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher_node',
         emulate_tty=True,
-        parameters=[{'use_sim_time':True, 'robot_description': Command(['xacro', robot_description_path])}],
-        output='screen'
+        parameters=[{'use_sim_time': True, 'robot_description': Command(['xacro ', robot_desc_path])}],
+        output="screen"
     )
 
+
+    # RVIZ Configuration
+    rviz_config_dir = os.path.join(get_package_share_directory(package_description), 'rviz', 'urdf_vis.rviz')
+
+
+    rviz_node = Node(
+            package='rviz2',
+            executable='rviz2',
+            output='screen',
+            name='rviz_node',
+            parameters=[{'use_sim_time': True}],
+            arguments=['-d', rviz_config_dir])
+
+    # create and return launch description object
     return LaunchDescription(
-        robot_state_publisher_node
-    ) 
+        [            
+            robot_state_publisher_node,
+            rviz_node
+        ]
+    )
--- src/puma560_gazebo/launch/spawn_robot_description.launch.py
+++ src/puma560_gazebo/launch/spawn_robot_description.launch.py
@@ -8,16 +8,20 @@ def generate_launch_description():
     orientation = [0.0, 0.0, 0.0]
     robot_name = "puma560"
 
+    print("func started")
+
     spawn_robot = Node(
-        package="gazebo_ros",
-        executable="spawn_entity.py",
-        name="spawn_entity",
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        name='spawn_entity',
         output='screen',
         arguments=['-entity', robot_name,
                    '-x', str(position[0]), '-y', str(position[1]), '-z', str(position[2]),
-                   '-R', str(orientation[0]), '-P', str(orientation[1], '-Y', str(orientation[2])),
+                   '-R', str(orientation[0]), '-P', str(orientation[1]), '-Y', str(orientation[2]),
                    '-topic', '/robot_description']
     )
+
+    print("Node acepted")
 
     return LaunchDescription(
         [
